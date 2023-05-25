3824398c82590f3ba9686059fcf6d6e4c7d434b2
harjas816 <harjas816@gmail.com>
2023-05-24 22:44:41 -0400

    complete

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+var multiply = num1 * num2
+
+var random = Math.ceil(Math.random() * 10)
+let num3 = 31
+let num4 = 9
+var mod = num3 % num4
+var bruh = [1,2,20]
+var max = Math.max(...bruh)
